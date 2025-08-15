2a50bbd7676b1019f99c2c7c2966965fa1feebe0
rathanachris <rathanachris239@gmail.com>
2025-08-15 21:52:43 +0700

    Create index.js
    Signed-off-by: rathanachris <rathanachris239@gmail.com>

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase/app';
+
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "myproject.firebaseapp.com",
+  const apiKey = process.env.FIREBASE_API_KEY;
+});
