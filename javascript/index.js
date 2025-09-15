be1864992aee5387c38e341b0a2eb376565a31f5
rathanachris <rathanachris239@gmail.com>
2025-09-16 01:12:06 +0700

    Delete javascript directory
    Signed-off-by: rathanachris <rathanachris239@gmail.com>

--- javascript/index.js
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase/app';
-
-firebase.initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "myproject.firebaseapp.com",
-  const apiKey = process.env.FIREBASE_API_KEY;
-});
