5fab0b3ae91780311c4331e44d1a1f36f201c16a
phonrathana <phonrathana198@gmail.com>
2025-11-06 04:31:12 +0700

    Create demoApplication.java
    Signed-off-by: phonrathana <phonrathana198@gmail.com>

---
+++ main.class/demoApplication.java
@@ -0,0 +1,11 @@
+package com.phonrathana.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(demoApplication.class, args);
+    }
+}
