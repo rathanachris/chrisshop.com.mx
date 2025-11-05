d9cebe4e59da6c0600e577ad8d6515141e610d94
phonrathana <phonrathana198@gmail.com>
2025-11-06 03:41:02 +0700

    Update System monitor.java
    Signed-off-by: phonrathana <phonrathana198@gmail.com>

--- service/System monitor.java
+++ service/System monitor.java
@@ -1,1 +1,12 @@
+package com.ai.controller.service;
 
+import java.util.Random;
+
+public class SystemMonitor {
+    public SystemStatus checkSystem() {
+        Random r = new Random();
+        double cpu = 20 + r.nextDouble() * 70;
+        double mem = 10 + r.nextDouble() * 80;
+        return new SystemStatus(cpu, 📉 📈 📊 mem);
+    }
+}
