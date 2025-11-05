b176064907611bdb4717dcde21102e20c3405d3a
phonrathana <phonrathana198@gmail.com>
2025-11-06 03:37:20 +0700

    Update and rename Aicontroller.java to Ai.Controller.java
    Signed-off-by: phonrathana <phonrathana198@gmail.com>

---
+++ Controller/Ai.Controller.java
@@ -0,0 +1,12 @@
+package com.ai.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiControllerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AiControllerApplication.class, args);
+        System.out.println("🚀 AI Controller REST API started on https://www.dnschrisrathana.com:443");
+    }
+}
